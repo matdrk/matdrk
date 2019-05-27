79d250c8b1b813d1ed27c6f2eca2231305060b5c
xdrkush <drkushikush@gmail.com>
2019-05-27 22:12:23 +0200

    user test

--- controllers/testPage.js
+++ controllers/testPage.js
@@ -4,10 +4,17 @@ const express = require('express')
     , router  = express.Router()
     , path    = require('path')
     , fs      = require('fs')
+    // DB
+    , dbUser  = require('../database/models/User')
+    
 
 router.get('/', async (req, res, next) => {
 
-    res.render('test')
+    const dbUsers = await dbUser.find({})
+
+    console.log({dbUsers});
+
+    res.render('test', {dbUsers})
 
 })
 
