1b105d6556dfb0c6110b3798d5e115c55b4d1b17
Ms2 <mathieu.sammiez@gmail.com>
2019-05-28 12:27:00 +0200

    revision routes

--- api/index.js
+++ api/index.js
@@ -1,34 +1,34 @@
-const express = require('express'),
-      router  = express.Router()
+const express = require('express')
+const router = express.Router()
 
-//controllers
-,     homePage    = require('./controllers/homePage')
-,     contactPage = require('./controllers/contactPage')
-,     blogPage    = require('./controllers/blogPage')
-,     mentionPage = require('./controllers/mentionPage')
-,     servicePage = require('./controllers/servicePage')
-,     loginPage   = require('./controllers/loginPage')
-,     loginRegister   = require('./controllers/loginRegister')
-,     cvPage      = require('./controllers/cvPage')
-,     adminPage   = require('./controllers/adminPage')
+    //controllers
+    , homePage = require('./controllers/homePage')
+    , contactPage = require('./controllers/contactPage')
+    , blogPage = require('./controllers/blogPage')
+    , mentionPage = require('./controllers/mentionPage')
+    , servicePage = require('./controllers/servicePage')
+    , loginPage = require('./controllers/loginPage')
+    , loginRegister = require('./controllers/loginRegister')
+    , cvPage = require('./controllers/cvPage')
+    , adminPage = require('./controllers/adminPage')
 
-,     testPage    = require('./controllers/testPage')
+    , testPage = require('./controllers/testPage')
 
 
 // Routes
-router.get('/'        , homePage)
-router.get('/contact' , contactPage)
-router.get('/blog'    , blogPage)
-router.get('/mention' , mentionPage)
-router.get('/service' , servicePage)
+router.get('/', homePage)
+router.get('/contact', contactPage)
+router.get('/blog', blogPage)
+router.get('/mention', mentionPage)
+router.get('/service', servicePage)
 //login
-router.get('/login'   , loginPage)
-router.post('/login/register' , loginRegister)
+router.get('/login', loginPage)
+router.post('/login/register', loginRegister)
 
 
-router.get('/admin'   , adminPage)
-router.get('/cv'      , cvPage)
+router.get('/admin', adminPage)
+router.get('/cv', cvPage)
 
-router.get('/test'    , testPage)
+router.get('/test', testPage)
 
 module.exports = router;
