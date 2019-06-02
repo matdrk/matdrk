8cfdc6e7253c763ed15761aed0bea7eb99515115
xdrkush <drkushikush@gmail.com>
2019-06-02 20:34:13 +0200

    filter div JS regler manque le css cont-app ...

--- api/controllers/testPage.js
+++ api/controllers/testPage.js
@@ -4,9 +4,12 @@ const express = require('express')
     , router  = express.Router()
     , path    = require('path')
     , fs      = require('fs')
+    , User    = require('../database/models/User')
+    , article = require('../database/models/Article')
 
     module.exports = async (req, res, next) => {
+        const dbUsers  = await User.find({})
+        ,     dbArticles = await article.find({}) 
 
-    res.render('test')
-
+    res.render('test', { dbUsers, dbArticles })
 }
