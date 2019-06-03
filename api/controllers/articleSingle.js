6240d8d88d07f3385c96c4f1dbda49330ff50ed8
Ms2 <mathieu.sammiez@gmail.com>
2019-06-03 22:26:39 +0200

    fix error navbar mobil

--- api/controllers/articleSingle.js
+++ api/controllers/articleSingle.js
@@ -2,9 +2,10 @@ const post = require("../database/models/Article");
 
 module.exports = async (req, res) => {
 
-    const dbArticles = await post.find({})
+    const dbArticles = await post.findById(req.params.id)
 
         console.log( {dbArticles} );
+        console.log( req.params );
         
     res.render('articles', { dbArticles })
 
