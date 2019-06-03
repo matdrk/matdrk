e6861494ee902995e41f4f7d28b36d89f9305719
Ms2 <mathieu.sammiez@gmail.com>
2019-06-03 16:47:55 +0200

    api test

--- api/controllers/cvPage.js
+++ api/controllers/cvPage.js
@@ -4,24 +4,22 @@ const express = require('express')
     , router  = express.Router()
     , path    = require('path')
     , fs      = require('fs')
-<<<<<<< HEAD:api/controllers/cvPage.js
     , pdfobject = require('pdfobject')
-=======
+
     // DB
     , dbUser  = require('../database/models/User')
     
->>>>>>> drk:controllers/testPage.js
+
 
     module.exports = async (req, res, next) => {
 
-<<<<<<< HEAD:api/controllers/cvPage.js
     res.render('cv')
-=======
+
     const dbUsers = await dbUser.find({})
 
     console.log({dbUsers});
 
     res.render('test', {dbUsers})
->>>>>>> drk:controllers/testPage.js
+
 
 }
