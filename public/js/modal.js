112f2a60a7f13231ff5e91b8d1f29e91edb29e2c
Ms2 <mathieu.sammiez@gmail.com>
2019-06-05 16:59:58 +0200

    log out modif zindex canvas

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,14 +1,14 @@
 $('document').ready(function(){
-    $('#button').on('click', function(){
-       $('.modal').addClass('modal-is-active');
-       $('.container').addClass('container-is-blurred');
-    });
-    
-      $('.modal').on('click', function(){
-          if($('.modal').hasClass('modal-is-active'))
-          {
-           $('.modal').removeClass('modal-is-active');
-           $('.container').removeClass('container-is-blurred');
-          }
-      });    
+  $('#buttonLog').on('click', function(){
+     $('.modal').addClass('modal-is-active');
+     $('.container').addClass('container-is-blurred');
   });
+  
+    $('.modal').on('click', function(){
+        if($('.modal').hasClass('modal-is-active'))
+        {
+         $('.modal').removeClass('modal-is-active');
+         $('.container').removeClass('container-is-blurred');
+        }
+    });    
+});
