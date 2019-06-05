b8d771f8c4a71ab7b24fb31f5a72e74c1c89f1be
xdrkush <drkushikush@gmail.com>
2019-06-05 21:08:39 +0200

    ok

--- api/controllers/articleSingle.js
+++ api/controllers/articleSingle.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 // Page Article
-=======
->>>>>>> 1355e3e6313f7cc841c185cb133238f1a9bc8b80
 const express = require('express')
     , app = express()
     , router = express.Router()
@@ -9,7 +6,6 @@ const express = require('express')
     , fs = require('fs')
     , User = require('../database/models/User')
     , post = require("../database/models/Article")
-<<<<<<< HEAD
     , comment = require("../database/models/Comment");
 
 router.get('/:id', async (req, res, next) => {
@@ -37,57 +33,3 @@ router.post('/comment', async (req, res, next) => {
 })
 
 module.exports = router;
-=======
-    , comment = require("../database/models/Comment")
-
-// module.exports = async (req, res) => {
-//     const dbArticles = await post.findById(req.params.id)
-//         console.log( {dbArticles} );
-//         console.log( req.params );
-//     res.render('articles', { dbArticles })
-// }
-
-router.get('/:id', async (req, res, next) => {
-    const dbArticles = await post.findById(req.params.id)
-        , dbComment  = await comment.find({})
-        , dbUser = await User.find({})
-        , sess = req.session;
-
-    console.log({ dbComment });
-    console.log(req.params);
-
-    res.render('articles', { dbArticles, sess, dbComment })
-})
-
-router.post('/comment', async (req, res, next) => {
-    comment.create(
-        req.body, (err, user) => {
-            if (req.session.status !== user) {
-                console.log('blabla');
-                res.redirect('/')
-            } else if ( req.session.status !== admin) {
-                res.redirect('/admin')
-            }
-        }
-    )
-})
-
-module.exports = router;
-
-router.post('/register', async (req, res, next) => {
-    User.create(
-        req.body, (error, user) => {
-            if (error) {
-                const registerError = Object.keys(error.errors).map(key => error.errors[key].message);
-                req.flash('registerError', registerError)
-                req.flash('data', req.body)
-                return res.redirect('/login')
-                console.log("test");
-
-            }
-            console.log(req.body);
-            res.redirect('/login')
-        }
-    )
-})
->>>>>>> 1355e3e6313f7cc841c185cb133238f1a9bc8b80
